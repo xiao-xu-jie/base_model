b2146ce7a862957efe4b1a87a56910c1b7fa7d7a
xiaoxualg <xiaoxualg@gitcode.net>
2024-09-13 18:05:22 +0800

    extend business and manager

---
+++ business/src/main/java/com/xujie/business/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.xujie.business.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Xujie
+ * @since 2024/9/13 17:40
+ **/
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ manager/src/main/java/com/xujie/ManagerApplication.java
@@ -0,0 +1,20 @@
+package com.xujie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @author Xujie
+ * @since 2024/9/13 18:00
+ **/
+@Slf4j
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class ManagerApplication {
+    public static void main(String[] args) {
+        log.info("ManagerApplication start...");
+        SpringApplication.run(ManagerApplication.class, args);
+        log.info("ManagerApplication start success...");
+    }
+}
