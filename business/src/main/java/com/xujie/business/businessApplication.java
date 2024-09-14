472d7074f4e9cd09884b1b51b396660b5e771211
xiaoxualg <xiaoxualg@gitcode.net>
2024-09-14 08:41:02 +0800

    init environment

--- business/src/main/java/com/xujie/business/businessApplication.java
+++ business/src/main/java/com/xujie/business/businessApplication.java
@@ -1,6 +1,8 @@
 package com.xujie.business;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -14,10 +16,15 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @Slf4j
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class businessApplication {
-    public static void main(String[] args) {
-        log.info("businessApplication === start");
 
+    public static String staticEnv;
+    public static void main(String[] args) {
+
+        log.info("businessApplication === start");
         SpringApplication.run(businessApplication.class, args);
         log.info("businessApplication === end");
+
+
     }
+
 }
