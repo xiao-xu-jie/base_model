ad227ec6baa797102026d91ff97e4ce45678c934
xiaoxualg <xiaoxualg@gitcode.net>
2024-09-16 09:42:11 +0800

    feat(http-starter): 初步完成http-starter配置
    完成http webclient配置

---
+++ public-denpendency/http-webclient/src/main/java/com/xujie/http/config/HttpConfig.java
@@ -0,0 +1,14 @@
+package com.xujie.http.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Xujie
+ * @since 2024/9/15 11:43
+ * Webclient 自动配置
+ **/
+
+
+@ComponentScan(value = "com.xujie.http")
+public class HttpConfig {
+}
