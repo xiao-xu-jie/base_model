f8fecb426ddccb47359c4054cb4142ead3ff5f22
xiaoxualg <xiaoxualg@gitcode.net>
2025-04-01 22:49:43 +0800

    去掉cors

--- business/src/main/java/com/xujie/business/config/CorsConfig.java
+++ business/src/main/java/com/xujie/business/config/CorsConfig.java
@@ -1,11 +1,10 @@
 package com.xujie.business.config;
 
-import java.util.Collections;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
 
 /**
  * CORS 资源共享配置
@@ -13,23 +12,23 @@ import org.springframework.web.filter.CorsFilter;
  * @author haoxr
  * @date 2022/10/24
  */
-@Configuration
+//@Configuration
 public class CorsConfig {
 
-  @Bean
-  public CorsFilter corsFilter() {
-    CorsConfiguration corsConfiguration = new CorsConfiguration();
-    // 1.允许任何来源
-    corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
-    // 2.允许任何请求头
-    corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
-    // 3.允许任何方法
-    corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
-    // 4.允许凭证
-    corsConfiguration.setAllowCredentials(true);
+    //  @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 1.允许任何来源
+        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
+        // 2.允许任何请求头
+        corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
+        // 3.允许任何方法
+        corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
+        // 4.允许凭证
+        corsConfiguration.setAllowCredentials(true);
 
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", corsConfiguration);
-    return new CorsFilter(source);
-  }
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
 }
