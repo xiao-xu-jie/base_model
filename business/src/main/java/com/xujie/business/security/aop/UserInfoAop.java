dc8791e7fbe5f2b89155e15ecb1a88a6a0c30bbe
xiaoxualg <xiaoxualg@gitcode.net>
2025-06-29 22:39:29 +0800

    feat(security): add Sa-Token dependencies and implement UserInfo AOP for user data handling

---
+++ business/src/main/java/com/xujie/business/security/aop/UserInfoAop.java
@@ -0,0 +1,39 @@
+package com.xujie.business.security.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Xujie
+ * @since 2025/4/25 23:56
+ **/
+@Aspect
+@Component
+public class UserInfoAop {
+
+    @Around("@annotation(com.xujie.business.security.annotations.UserInfo)")
+    public Object handleMyAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        // 找到对应用户的参数
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof Object) {
+                args[i] = getUserInfoData();
+            }
+        }
+        Object result;
+        try {
+            result = joinPoint.proceed(args);
+        } catch (Exception e) {
+            throw e;
+        }
+        return result;
+    }
+
+    private Object getUserInfoData() {
+        // 模拟获取用户信息
+
+        return null;
+    }
+}
---
+++ business/src/main/java/com/xujie/business/security/utils/SecurityUtil.java
@@ -0,0 +1,11 @@
+package com.xujie.business.security.utils;
+
+/**
+ * @author Xujie
+ * @since 2025/4/25 23:53
+ **/
+
+
+public class SecurityUtil {
+
+}
